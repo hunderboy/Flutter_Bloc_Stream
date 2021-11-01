b2bb72c312cf7b15246600bdab4a6546bddf633f
hunderboy <masury2011@gmail.com>
2021-11-01 16:44:38 +0900

    bloc 작성 완료

---
+++ lib/src/bloc/Bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class Bloc{
+  Set<WordPair> saved = Set<WordPair>();
+
+  final _savedController = StreamController<Set<WordPair>>();
+
+  get savedListStream => _savedController.stream;
+
+  // 데이터 추가 or 삭제
+  addToOrRemoveFromSavedList(WordPair item){
+    if(saved.contains(item)){
+      saved.remove(item);
+    }else{
+      saved.add(item);
+    }
+
+    _savedController.sink.add(saved);
+  }
+
+
+  dispose(){  // 메모리 릭이 발생하지 않게
+    _savedController.close(); // 메모리에서 해제
+  }
+}
+
+
+var bloc = Bloc(); // static 으로 만드는 방식.
